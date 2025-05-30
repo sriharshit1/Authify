0207ce50b4cfee247962493a603e21afbba27796
sriharshit1 <srivastavaharshit196@gmail.com>
2025-05-30 13:07:00 +0530

    Modifying AuthifyApplication.java

--- AuthBackend/src/main/java/com/Harshit/authify/AuthifyApplication.java
+++ AuthBackend/src/main/java/com/Harshit/authify/AuthifyApplication.java
@@ -7,14 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthifyApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(AuthifyApplication.class, args);
-		try {
-			SpringApplication.run(AuthifyApplication.class, args);
-			System.out.println("✅ Authify is running");
-		} catch (Throwable t) {
-			System.err.println("❌ Startup failed:");
-			t.printStackTrace();
-		}
+		SpringApplication.run(AuthifyApplication.class, args);
+
 	}
 
 }
