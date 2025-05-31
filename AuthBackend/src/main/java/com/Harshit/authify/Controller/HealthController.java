97c89109491252af295ca49e5631e329aaed0ca0
sriharshit1 <srivastavaharshit196@gmail.com>
2025-05-31 13:36:17 +0530

    modifying main class

--- AuthBackend/src/main/java/com/Harshit/authify/Controller/HealthController.java
+++ AuthBackend/src/main/java/com/Harshit/authify/Controller/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-    @GetMapping("/check")
+    @GetMapping("/")
     public String healthCheck() {
         return "Backend is up!";
     }
