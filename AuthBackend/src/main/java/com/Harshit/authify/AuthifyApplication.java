9ce49ff24dd3171a3e82e76e7bd9ca4853130562
sriharshit1 <srivastavaharshit196@gmail.com>
2025-05-31 13:33:39 +0530

    adding some bean

--- AuthBackend/src/main/java/com/Harshit/authify/AuthifyApplication.java
+++ AuthBackend/src/main/java/com/Harshit/authify/AuthifyApplication.java
@@ -1,7 +1,10 @@
 package com.Harshit.authify;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AuthifyApplication {
@@ -9,6 +12,11 @@ public class AuthifyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthifyApplication.class, args);
 
+	}
+
+	@Bean
+	public CommandLineRunner logPort(@Value("${server.port}") String port) {
+		return args -> System.out.println("✅ Server is running on port: " + port);
 	}
 
 }
