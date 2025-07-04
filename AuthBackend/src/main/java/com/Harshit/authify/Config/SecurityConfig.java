f9a60933b4339d63b9c7f2379e232dffd592b3cb
sriharshit1 <srivastavaharshit196@gmail.com>
2025-07-04 11:46:39 +0530

    Adding frontend url

--- AuthBackend/src/main/java/com/Harshit/authify/Config/SecurityConfig.java
+++ AuthBackend/src/main/java/com/Harshit/authify/Config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
 
     private CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://authify-ne6v.onrender.com","http://localhost:5173"));
         config.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization","Content-Type"));
         config.setAllowCredentials(true);
