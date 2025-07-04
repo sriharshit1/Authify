802aeb8eb4d61453350972a860bc8a01360398b6
sriharshit1 <srivastavaharshit196@gmail.com>
2025-07-04 11:37:07 +0530

    adding backend url

--- AuthFrontend/src/util/constants.jsx
+++ AuthFrontend/src/util/constants.jsx
@@ -1,3 +1,4 @@
 export const AppConstants={
-    BACKEND_URL : 'http://localhost:8080/api/v1.0',
+    // BACKEND_URL : 'http://localhost:8080/api/v1.0',
+    BACKEND_URL : 'https://authify-kxfr.onrender.com/api/v1.0',
 }
